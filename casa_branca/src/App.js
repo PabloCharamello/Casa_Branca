0720cedb519a384b65a9a5b45a65a56f2c379640
Pablo Charamello <pablocharamello@gmail.com>
2022-11-20 03:27:18 -0300

    base project ready

--- casa_branca/src/App.js
+++ casa_branca/src/App.js
@@ -1,7 +1,15 @@
+import "./index.css";
 import React from "react";
+import Navbar from "./components/Navbar";
+import Nuevo from "./components/Nuevo";
 
 const App = () => {
-  return <div>Testing</div>;
+  return (
+    <div className="w-screen h-auto flex flex-col">
+      <Navbar />
+      <Nuevo />
+    </div>
+  );
 };
 
 export default App;
---
+++ casa_branca/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      {/* desktop and tablet */}
+      <div className="">123</div>
+      {/* mobile */}
+      <div className="flex md:hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;
