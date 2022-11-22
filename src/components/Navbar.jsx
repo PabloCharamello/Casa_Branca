a0eac0f292d78a2840ccc51feee8bdba1f924d0b
Pablo Charamello <pablocharamello@gmail.com>
2022-11-22 03:28:39 -0300

    file extension images changed and compressed to webp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,17 +37,23 @@ const Navbar = () => {
     <header className="fixed z-50 w-screen p-2 px-3 md:p-5 md:px-16">
       {/* desktop and tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <Link to={"/"} className="flex items-center gap-2">
+        <motion.Link
+          to={"/"}
+          initial={{ opacity: 0, y: -80, duration: 1500 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 100 }}
+          className="flex items-center gap-2"
+        >
           <img
             src={Logo}
             alt="ecommerce Logo"
             className="w-10 drop-shadow-lg"
           />
           <p className="text-headingColor text-xl font-bold">Casa Branca</p>
-        </Link>
+        </motion.Link>
         <div className="flex items-center gap-8">
           <motion.ul
-            initial={{ opacity: 0, x: 200 }}
+            initial={{ opacity: 0, x: 200, duration: 1500 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-8"
@@ -87,7 +93,7 @@ const Navbar = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className="w-36 bg-gray-50 shadow-x1 rounded-lg absolute flex flex-col top-11 right-0"
               >
-                {user && user.email === "pablocharamello2@gmail.com" && (
+                {user && user.email === "pablocharamello@gmail.com" && (
                   <Link to="./createItem">
                     <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
                       New Item <MdAdd />
@@ -104,7 +110,12 @@ const Navbar = () => {
       </div>
       {/* mobile */}
 
-      <div className="flex md:hidden w-full h-full">
+      <motion.div
+        className="flex md:hidden w-full h-full"
+        initial={{ opacity: 0, y: -80, duration: 1500 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 100 }}
+      >
         <Link to={"/"} className="flex items-center gap-2">
           <img
             src={Logo}
@@ -113,7 +124,7 @@ const Navbar = () => {
           />
           <p className="text-headingColor text-xl font-bold">Casa Branca</p>
         </Link>
-      </div>
+      </motion.div>
     </header>
   );
 };
